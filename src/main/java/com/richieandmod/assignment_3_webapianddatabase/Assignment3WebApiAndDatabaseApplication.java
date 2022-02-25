e59bafa89ba195a65b9209a559237048614fa900
iljaasd <iljaasd@gmail.com>
2022-02-25 15:06:27 +0100

    Initial commit

---
+++ src/main/java/com/richieandmod/assignment_3_webapianddatabase/Assignment3WebApiAndDatabaseApplication.java
@@ -0,0 +1,13 @@
+package com.richieandmod.assignment_3_webapianddatabase;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Assignment3WebApiAndDatabaseApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Assignment3WebApiAndDatabaseApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/richieandmod/assignment_3_webapianddatabase/Assignment3WebApiAndDatabaseApplicationTests.java
@@ -0,0 +1,13 @@
+package com.richieandmod.assignment_3_webapianddatabase;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Assignment3WebApiAndDatabaseApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
