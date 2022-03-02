bd89e6699312fa4e88dd0a38ddb81d7a0720de47
Richie Schuurman <richie.schuurman@hotmail.com>
2022-03-02 17:36:36 +0100

    Added FranchiseService with new getters and update

--- src/main/java/com/richieandmod/assignment_3_webapianddatabase/Controllers/FranchiseController.java
+++ src/main/java/com/richieandmod/assignment_3_webapianddatabase/Controllers/FranchiseController.java
@@ -4,6 +4,7 @@ import com.richieandmod.assignment_3_webapianddatabase.Models.CommonResponse;
 import com.richieandmod.assignment_3_webapianddatabase.Models.Franchise;
 import com.richieandmod.assignment_3_webapianddatabase.Models.Movie;
 import com.richieandmod.assignment_3_webapianddatabase.Repositories.FranchiseRepository;
+import com.richieandmod.assignment_3_webapianddatabase.Services.FranchiseServiceImpl;
 import com.richieandmod.assignment_3_webapianddatabase.Utilities.Command;
 import com.richieandmod.assignment_3_webapianddatabase.Utilities.Logger;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -13,6 +14,7 @@ import org.springframework.web.bind.annotation.*;
 
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import java.util.List;
 import java.util.Optional;
 
 @RestController
@@ -21,6 +23,9 @@ public class FranchiseController {
 
     @Autowired
     FranchiseRepository franchiseRepository;
+
+    @Autowired
+    private FranchiseServiceImpl franchiseServiceImpl;
 
     //Get all franchises
     @GetMapping("/all")
@@ -61,10 +66,60 @@ public class FranchiseController {
         return new ResponseEntity<>(commonResponse, resp);
     }
 
+    //Get all movies in a franchise
+    @GetMapping("/{name}/movies")
+    public ResponseEntity<CommonResponse> getAllMoviesInFranchiseByTitle(HttpServletRequest request,
+                                                               @PathVariable String name) {
+        Command cmd = new Command(request);
+
+        CommonResponse commonResponse = new CommonResponse();
+        HttpStatus resp;
+        List<String> movieNames;
+
+        if(franchiseRepository.existsFranchiseByName(name)) {
+            movieNames = franchiseServiceImpl.getAllMoviesInFranchise(name);
+            commonResponse.data = movieNames;
+            commonResponse.message = "All movies published by franchise: " + name;
+            resp = HttpStatus.OK;
+        } else {
+            commonResponse.data = null;
+            commonResponse.message = "Franchise not found";
+            resp = HttpStatus.NOT_FOUND;
+        }
+
+        cmd.setResult(resp);
+        Logger.getInstance().logCommand(cmd);
+        return new ResponseEntity<>(commonResponse, resp);
+    }
+
+
+    //Update movies in franchise
+    @PutMapping("/{id}/movies/update/")
+    public ResponseEntity<CommonResponse> updateMoviesInFranchise(HttpServletRequest request,
+                                                                  @PathVariable Integer id, @RequestBody Integer[] franchiseId) {
+        Command cmd = new Command(request);
+        CommonResponse commonResponse = new CommonResponse();
+        HttpStatus resp;
+
+        if (franchiseRepository.existsById(id)) {
+            commonResponse.data = franchiseServiceImpl.updateMoviesInFranchise(id, franchiseId);
+            commonResponse.message = "movies in franchise with id: " + id + " have been updated";
+            resp = HttpStatus.OK;
+        } else {
+            commonResponse.data = null;
+            commonResponse.message = "movie not found";
+            resp = HttpStatus.NOT_FOUND;
+        }
+
+        cmd.setResult(resp);
+        Logger.getInstance().logCommand(cmd);
+        return new ResponseEntity<>(commonResponse, resp);
+    }
+
     //Create franchise and save to DB
     @PostMapping("/create")
     public ResponseEntity<CommonResponse> createFranchise(HttpServletRequest request, HttpServletResponse response,
-                                                     @RequestBody Franchise franchise) {
+                                                          @RequestBody Franchise franchise) {
         Command cmd = new Command(request);
 
         franchise = franchiseRepository.save(franchise);
@@ -85,7 +140,7 @@ public class FranchiseController {
     //Update existing franchise by id
     @PutMapping("/update/{id}")
     public ResponseEntity<CommonResponse> updateFranchise(HttpServletRequest request, @PathVariable Integer id,
-                                                     @RequestBody Franchise newFranchise) {
+                                                          @RequestBody Franchise newFranchise) {
         Command cmd = new Command(request);
 
         CommonResponse commonResponse = new CommonResponse();
--- src/main/java/com/richieandmod/assignment_3_webapianddatabase/Repositories/FranchiseRepository.java
+++ src/main/java/com/richieandmod/assignment_3_webapianddatabase/Repositories/FranchiseRepository.java
@@ -1,7 +1,11 @@
 package com.richieandmod.assignment_3_webapianddatabase.Repositories;
 
 import com.richieandmod.assignment_3_webapianddatabase.Models.Franchise;
+import com.richieandmod.assignment_3_webapianddatabase.Models.Movie;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface FranchiseRepository extends JpaRepository<Franchise, Integer> {
+    boolean existsFranchiseByName(String name);
+    Franchise findFirstByName(String name);
+
 }
